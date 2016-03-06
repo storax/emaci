d9285e08d190051fc2576645a950bb515346e495
David Zuber <zuber.david@gmx.de>
2016-03-06 21:36:25 +0000

    Project skeleton

---
+++ emaci.el
@@ -0,0 +1,31 @@
+;;; emaci.el --- scheduler for compilations in emacs
+
+;; Copyright (C) 2016 by David ZUBER
+
+;; Author: David ZUBER <zuber.david@gmx.de>
+;; URL: https://github.com/storax/emaci
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; emaci provides a scheduler to queue compilation jobs and execute them automatically.
+
+;;; Code:
+
+(provide 'emaci)
+
+;;; emaci.el ends here
