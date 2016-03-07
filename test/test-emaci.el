bc9467126dc0fc86e7fd06609e4fbc8df0ff72f1
David Zuber <zuber.david@gmx.de>
2016-03-07 22:06:53 +0000

    Add tests

--- test/test-emaci.el
+++ test/test-emaci.el
@@ -24,8 +24,135 @@
 (require 'ert)
 (require 'emaci)
 
-(ert-deftest example ()
-  "Example test"
-  (should (equal 1 1)))
+(defmacro with-sandbox (&rest body)
+  "Evaluate BODY with all variables let bound."
+  `(let ((emaci-queue nil)
+        (emaci-history nil)
+        (emaci--buffer-job-alist nil)
+        (emaci--build-counter 0))
+     ,@body))
+
+(ert-deftest get-buildno ()
+  "Test getting buildno."
+  (with-sandbox
+  (let ((result (emaci//get-buildno)))
+    (should (equal result 1)))))
+
+(ert-deftest get-buildno-increase ()
+  "Test getting buildno."
+  (with-sandbox
+   (let ((emaci--build-counter 1))
+    (emaci//get-buildno)
+    (should (equal emaci--build-counter 2)))))
+
+(defun test-job ()
+  "Create a test job."
+  (emaci//new-job "~" "echo Tis but a scratch" 'comint-mode "$.*^"))
+
+(ert-deftest new-job-buildno-first ()
+  "Test if first job has right build-no."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-buildno job) 1)))))
+
+(ert-deftest new-job-buildno-second ()
+  "Test if first job has right build-no."
+  (with-sandbox
+   (let ((job (test-job))
+         (job2 (test-job)))
+     (should (equal (emaci-job-buildno job2) 2)))))
+
+(ert-deftest new-job-build-counter ()
+  "Test if counter is increased after job creation."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal emaci--build-counter 1)))))
+
+(ert-deftest new-job-status ()
+  "Test initial job status."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-status job) 'queued)))))
+
+(ert-deftest new-job-statusmsg ()
+  "Test initial job statusmsg."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-statusmsg job) nil)))))
+
+(ert-deftest new-job-buffer ()
+  "Test initial job buffer."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-buffer job) nil)))))
+
+(ert-deftest new-job-dir ()
+  "Test job dir attribute."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-dir job) "~")))))
+
+(ert-deftest new-job-command ()
+  "Test job command attribute."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-command job)
+                    "echo Tis but a scratch")))))
+
+(ert-deftest new-job-mode ()
+  "Test job mode attribute."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-mode job) 'comint-mode)))))
+
+(ert-deftest new-job-highlight-regexp ()
+  "Test job highlight regexp attribute."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal (emaci-job-highlight-regexp job) "$.*^")))))
+
+(ert-deftest new-job-queue ()
+  "Test if queue stays empty when creating a job."
+  (with-sandbox
+   (let ((job (test-job)))
+     (should (equal emaci-queue nil)))))
+
+(ert-deftest queue-job-one ()
+  "Test if a job gets queued."
+  (with-sandbox
+   (let ((job (test-job)))
+     (emaci//queue-job job)
+     (should (equal emaci-queue (list job))))))
+
+(ert-deftest queue-job-two ()
+  "Test if job gets appended to queue."
+  (with-sandbox
+   (let ((job (test-job))
+         (job2 (test-job)))
+     (emaci//queue-job job)
+     (emaci//queue-job job2)
+     (should (equal emaci-queue (list job job2))))))
+
+(ert-deftest running-job-p-empty ()
+  "Test there is no running job in an empty queue."
+  (with-sandbox
+     (should-not (emaci//running-job-p))))
+
+(ert-deftest running-job-p-not-running ()
+  "Test if queued job is not running."
+  (with-sandbox
+   (let ((job (test-job)))
+     (emaci//queue-job job)
+     (should-not (emaci//running-job-p)))))
+
+(ert-deftest running-job-p-running ()
+  "Test if there is a running job in queue."
+  (with-sandbox
+   (let ((job (test-job))
+         (job2 (test-job)))
+     (setf (emaci-job-status job) 'running)
+     (emaci//queue-job job)
+     (emaci//queue-job job2)
+     (should (emaci//running-job-p)))))
 
 ;;; test-emaci.el ends here
